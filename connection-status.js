5dbb94d7fc51cb36bd627ba868a09363c66c9bf5
mae-boittiaux <maeboittiaux@gmail.com>
2025-07-16 13:35:07 +0100

    feat(jsdoc): add jsdoc to file 'connection-status.js'

--- connection-status.js
+++ connection-status.js
@@ -1,5 +1,23 @@
+/**
+ * @fileoverview 
+ * Connection Status module for monitoring and updating the network connectivity status.
+ * 
+ * The following script listens to the online and offline events of the browser in order 
+ * to update the network status displayed on the page, dynamically setting the text of 
+ * the element with ID: 'connectionStatus' based on the network status.
+ */
+
 import { logMessage, MessageScope, MessageOutput } from './log-message.js';
 
+/**
+ * Sets up the network connectivity status monitoring.
+ * 
+ * This function retrieves the DOM element with the ID 'connectionStatus' and dynamically
+ * updates its text content, and attaches event listeners to the 'online' and 'offline' 
+ * events, ensuring that the element is updated whenever the connection status changes.
+ *
+ * @returns {void}
+ */
 export function setupConnectionStatus() {
     const connectionStatus = document.getElementById('connectionStatus');
 
