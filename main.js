c69f09ea8f28b3c50cf67a07caa05b78aca693fd
mae-boittiaux <maeboittiaux@gmail.com>
2025-07-17 15:02:33 +0100

    feat(initialisation): add reference to 'initialisation.js' within main entry point of application

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+import { initialiseApp } from './initialisation.js';
 import { registerServiceWorker } from './registration.js';
 import { setupConnectionStatus } from './connection-status.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
+    await initialiseApp();
     registerServiceWorker();
     setupConnectionStatus();
 });
