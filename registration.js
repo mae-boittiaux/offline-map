e25b8ffd3b7e084ab4bfb618d5d94d1c7bdc9c14
mae-boittiaux <maeboittiaux@gmail.com>
2025-07-17 15:07:44 +0100

    feat(jsdoc): add jsdoc to file 'registration.js'

--- registration.js
+++ registration.js
@@ -1,5 +1,24 @@
+/**
+ * @fileoverview 
+ * Handles the registration of the Service Worker.
+ * 
+ * The following module exports a function that registers the Service Worker for the 
+ * application.  It verifies if there is browser support for Service Workers, and then 
+ * logs the registration status to the console.
+ */
+
 import { logMessage, MessageScope, MessageOutput } from './log-message.js';
 
+/**
+ * Registers the Service Worker for the application.
+ * 
+ * Checks for Service Worker support in the current browser environment; if supported, 
+ * attempts to register the Service Worker from './service-worker.js'. Upon successful 
+ * registration, it logs the registration scope; otherwise, it logs an error.
+ *
+ * @returns {Promise<void>} A promise that resolves when the Service Worker registration 
+ *                          process completes.
+ */
 export async function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         try {
